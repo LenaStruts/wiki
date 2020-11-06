48b24131076899367985dc4d61f9397e20ebabe7
LenaStruts <lena19struts@gmail.com>
2020-11-06 16:59:03 +0100

    add entry page functionality

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -8,3 +8,15 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+def entry(request, title):
+    entry_page = util.get_entry(title)
+    if entry_page is None:
+        return render(request, "encyclopedia/non.html", {
+            "entry_title": title    
+        })
+    else:
+        return render(request, "encyclopedia/entry.html", {
+            "entry": markdowner.convert(entry_page),
+            "entry_title": title
+        })
